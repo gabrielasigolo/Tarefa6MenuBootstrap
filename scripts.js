35f256afd3de1f83afc06b7f3ae5fec00a0a4a90
gabrielasigolo <gabrielasigolo12@gmail.com>
2022-04-05 10:57:26 -0300

    finalizando o front

---
+++ scripts.js
@@ -0,0 +1,33 @@
+var buttonPlus  = $(".cart-qty-plus");
+var buttonMinus = $(".cart-qty-minus");
+
+var incrementPlus = buttonPlus.click(function() {
+	var $n = $(this)
+		.parent(".counter")
+		.parent(".card-body")
+		.find(".qty");
+	$n.val(Number($n.val())+1 );
+});
+
+var incrementMinus = buttonMinus.click(function() {
+		var $n = $(this)
+		.parent(".counter")
+		.parent(".card-body")
+		.find(".qty");
+	var amount = Number($n.val());
+	if (amount > 0) {
+		$n.val(amount-1);
+	}
+});
+
+function Calc() {
+    
+}
+
+/*var counter = $(".quantity");
+
+    for(var i = 0; i <= counter.length; i++){
+        if(counter[i] == 0){
+
+        }
+    }*/
